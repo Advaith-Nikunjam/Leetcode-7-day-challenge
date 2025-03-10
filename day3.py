7ae5225b5128fa92e13985116618292941e3a113
Advaith Krishna <advaithnikunjam@gmail.com>
2025-03-10 19:37:45 +0530

    Update day3.py

--- day3.py
+++ day3.py
@@ -1,16 +1,5 @@
 class Solution(object):
     def findMaxAverage(self, nums, k):
-        # max_sum = float('-inf')
-        # for i in range(len(nums) - k + 1):
-        #     current_sum = 0 
-        #     for j in range(i, i + k):
-        #         current_sum += nums[j] 
-        #     if current_sum > max_sum:
-        #         max_sum = current_sum
-        # average = max_sum / k 
-        # return average
-        
-        
         current_sum = sum(nums[:k])
         max_sum = current_sum
         for i in range(k,len(nums)):
